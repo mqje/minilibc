fed123728037a73aab435205b263aee41a030b6f
maje <marin.brunel@epitech.eu>
2018-03-02 18:43:48 +0100

    Test

---
+++ src/memset.asm
@@ -0,0 +1,28 @@
+	[BITS 64]               	; Obligatoire
+
+	global omemset  ;  initialize the name of the function Obligatoire
+	section .text   ; // set the .text Obligatoire
+
+omemset:
+	push rbp        ; Initialize stack
+	mov rbp, rsp    ; Initialize stack
+	mov r14, rdi
+
+loop:
+	mov al, [rdi]	; assign cl (register) with the first parameter of the function
+
+	cmp rdx, 0	; compare the two value of the character stocked in the cl and dl register
+	je end		; if the value are different, go to the end, because its an error
+
+	cmp rdi, 0
+	je end 
+	
+	mov [rdi], sil
+	sub rdx, 1
+	inc rdi         ; increment rdi (1st operand)
+	jmp loop        ; call loop (recursive)
+
+end:
+	mov rax, r14    ; assign cl to rax
+	leave		; free the stack
+	ret             ; return obligatoire 
